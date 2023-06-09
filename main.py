f863f06f0225aff192bc353976fc0188a83cd341
Alfonso Núñez <78993533+00anp@users.noreply.github.com>
2023-06-09 17:43:23 -0600

    Import pandas and render the titles from data.csv

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 
 st.set_page_config(layout="wide")
@@ -24,3 +25,15 @@ with col2:
 
 call_to_action = "Below you can find some of the apps I have built. Feel free to contact me."
 st.write(call_to_action)
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv("data.csv", sep=";")
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+
